8647574f6d32bf3a9b21233b779c7b33d6e91f99
MACARENA-PC\Macarena <macapineiro@gmail.com>
2018-09-09 16:19:00 -0300

    ss
    ss

--- 404.php
+++ 404.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>The Kooks | Error</title>
-	<link rel="stylesheet" type="text/css" href="css/estilo-404.css">
+	<title>The Kooks</title>
+	<link rel="stylesheet" type="text/css" href="estilo-404.css">
 </head>
 <body>
 
 	<section>
 		<article>
-			<h1>404 Error Page #1</h1>
-				<p class="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
+			<h1>The Kooks Band</h1>
+				<p class="zoom-area"><b>Oops!</b> Error al cargar la página. </p>
 			<section class="error-container">
 			  <span><span>4</span></span>
 			  <span>0</span>
 			  <span><span>4</span></span>
 			</section>
 			<div class="link-container">
-			  <a target="_blank" href="https://www.silocreativo.com/en/creative-examples-404-error-css/" class="more-link">Visit the original article</a>
+			  <a target="_blank" href="index.html" class="more-link">Volver a página principal</a>
 			</div>
 		</article>
 	</section>
